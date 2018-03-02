36e54986a00f13ba36eca24adc49bdc551d4d1f4
whiteblue pants <915366232@qq.com>
2018-03-02 17:41:26 +0800

    code

--- P1847.c
+++ P1847.c
@@ -16,6 +16,25 @@ int main()
     }
     for(int i = 0;i < m;i ++)
     {
-        
+        for(int j = 0;j < n;j ++)
+        {
+            if(t[j][0] >= p[i][0] && t[j][0] <= p[i][2] && t[j][1] >= p[i][1] && t[j][1] <= p[i][3])
+            {
+                t[j][2] ++;
+                t[j][3] = i + 1;
+            }
+        }
     }
+    for(int i = 0;i < n;i ++)
+    {
+        if(t[i][2] == 0)
+        {
+            printf("NO\n");
+        }
+        else
+        {
+            printf("YES %d %d\n",t[i][2],t[i][3]);
+        }
+    }
+    return 0;
 }
